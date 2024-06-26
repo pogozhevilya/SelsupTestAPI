31aab1165bc04d584ecd8da1c837e5085afbe47b
Ilya Pogozhev <llya.warspear@mail.ru>
2024-06-26 19:56:15 +0300

    init project

--- src/main/java/CrptApi.java
+++ src/main/java/CrptApi.java
@@ -4,7 +4,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
